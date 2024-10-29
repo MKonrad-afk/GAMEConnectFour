9c2d8663bb53917adf55b9771ddf41d8abd1152b
Mkonrad-afk <s29778@pjwstk.edu.pl>
2024-10-29 18:02:25 +0100

    methods: hortizontalTest0row hortizontalTest1row verticalTest0column verticalTest1column added

--- src/test/WindowUnitTests.java
+++ src/test/WindowUnitTests.java
@@ -1,20 +1,109 @@
 package test;
 
-import org.junit.jupiter.api.Test;
+import main.Window;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class WindowUnitTests {
 
+    Window win;
+    @BeforeEach
+    public void preSetup(){
+        try {
+            System.loadLibrary("libUTP_Project");
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Failed to load native library: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            win = new Window();
+        } catch (IOException e) {
+            System.err.println("IOException encountered: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
-    public void horizonralWinCondition(){
-
+    public void horizonralWinCondition0Row(){
+        win.addAfterAcepting();//first player first token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(1);//column go right
+        win.addAfterAcepting();//first player second token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(1);//column go right
+        win.addAfterAcepting();//first player third token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(1);//column go right
+        win.addAfterAcepting();//first player fourth token
+        assertTrue( "Horizontal win condition failed", win.checkIfWIN());
     }
 
     @Test
-    public void vertivalWinCondition(){
-
+    public void horizontalWinCondition1Row(){
+        //filling the 0th row
+        for (int i =0; i< 7; i++){
+            win.addAfterAcepting();
+            win.changeTheColumn(1);
+        }
+        win.changeTheColumn(0);//go left
+        win.addAfterAcepting();//first player first token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(0);//go left
+        win.addAfterAcepting();//first player second token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(0);//go left
+        win.addAfterAcepting();//first player third token
+        win.addAfterAcepting();//second player on the top
+        win.changeTheColumn(0);//go left
+        win.addAfterAcepting();//first player fourth token
+        assertTrue( "Horizontal win condition failed", win.checkIfWIN());
     }
 
+
+    @Test
+    public void verticalWinCondition0Column(){
+        win.addAfterAcepting();//first player first token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player first token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player second token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player second token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player third token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player third token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player fourth token
+        assertTrue( "Vertical win condition failed", win.checkIfWIN());
+    }
+
+    @Test
+    public void verticalWinCondition1Column(){
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//first player first token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player first token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player second token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player second token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player third token
+        win.changeTheColumn(1);// go right
+        win.addAfterAcepting();//second player third token
+        win.changeTheColumn(0);// go left
+        win.addAfterAcepting();//first player fourth token
+        assertTrue( "Vertical win condition failed", win.checkIfWIN());
+    }
     @Test
     public void diagonalWinCondition_Right(){
 
